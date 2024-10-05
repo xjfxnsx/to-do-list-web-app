c00b3db1f57d2107843e566cc07e40fee0ba0d7d
Dzhafar Nasirov <xjfxnsx@gmail.com>
2024-10-05 23:07:52 +0400

    added tasks to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Column from './components/Column';
 
+interface Task {
+  id: number;
+  title: string;
+  description: string;
+  deadline: string;
+}
+
 const App: React.FC = () => {
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      id: 1,
+      title: 'Learn TypeScript',
+      description: 'Pass tutorial and documentation.',
+      deadline: '2024-10-10'
+    },
+    {
+      id: 2,
+      title: 'Create To-Do App',
+      description: 'Create project with columns and cards.',
+      deadline: '2024-10-15'
+    },
+  ]);
+
   return (
-    <div style = {
-      {display: 'flex',
+    <div style={
+      {
+        display: 'flex',
         justifyContent: 'space-around'
       }
     }>
-      <Column title='Task' />
-      <Column title='In Process' />
-      <Column title='Done' />
+      <Column title='Task' tasks={tasks}/>
+      <Column title='In Process' tasks={[]}/>
+      <Column title='Done' tasks={[]}/>
     </div>
   );
 };
