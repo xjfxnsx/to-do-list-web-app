dac1a5c859f54a2b40d12c5ba6501620e5551dcb
Dzhafar Nasirov <xjfxnsx@gmail.com>
2024-10-17 23:21:26 +0400

    added localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Column from './components/Column';
-import { DragDropContext, Droppable, Draggable, DropResult } from 'react-beautiful-dnd';
+import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 import './App.css';
 
 interface Task {
@@ -11,27 +11,36 @@ interface Task {
 }
 
 const App: React.FC = () => {
-  const [tasks, setTasks] = useState<Task[]>([
-    {
-      id: 1,
-      title: 'Learn TypeScript',
-      description: 'Pass tutorial and documentation.',
-      deadline: '2024-10-10'
-    },
-    {
-      id: 2,
-      title: 'Create To-Do App',
-      description: 'Create project with columns and cards.',
-      deadline: '2024-10-15'
-    },
-  ]);
+  const [tasks, setTasks] = useState<Task[]>(() => {
+
+    const savedTasks = localStorage.getItem('tasks');
+    return savedTasks ? JSON.parse(savedTasks) : [
+
+      {
+        id: 1,
+        title: 'Learn TypeScript',
+        description: 'Pass tutorials and documentation.',
+        deadline: '2024-10-10'
+      },
+      {
+        id: 2,
+        title: 'Create To-Do App',
+        description: 'Create project with columns and cards.',
+        deadline: '2024-10-15'
+      },
+    ];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
 
   const onDragEnd = (result: DropResult) => {
     const { source, destination } = result;
-    
-    if (!destination || 
+
+    if (!destination ||
       (source.droppableId === destination.droppableId &&
-    source.index === destination.index)) {
+        source.index === destination.index)) {
       return;
     }
 
